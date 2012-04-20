e7eed87978b1a799644b16ae806ff0204c680cc3
wakabatan <wakabatan@localhost.localdomain>
2012-04-20 12:47:38 +0900

    Newline cleanup

--- lib/Web/UserAgent/Functions.pm
+++ lib/Web/UserAgent/Functions.pm
@@ -186,6 +186,7 @@ sub _http {
     if ($args{basic_auth}) {
         require MIME::Base64;
         $args{header_fields}->{'Authorization'} ||= 'Basic ' . MIME::Base64::encode_base64(encode 'utf-8', ($args{basic_auth}->[0] . ':' . $args{basic_auth}->[1]));
+        $args{header_fields}->{'Authorization'} =~ s/\x0D\x0A/ /g;
     }
 
     while (my ($n, $v) = each %{$args{header_fields} or {}}) {
@@ -250,6 +251,7 @@ sub _http {
             $args{url},
             body => $req->content,
             headers => {
+                map { s/[\x0D\x0A]/ /g; $_ }
                 map { ( $_ => $req->header($_) ) } $req->header_field_names
             },
             sub {
